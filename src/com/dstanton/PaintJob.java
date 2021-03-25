bf75f74f34745fe862a83ed0cf85f955e81e7cc3
dstanton <djstanton@gmail.com>
2021-03-25 08:38:15 -0700

    Added new overload method for getBucketCount with only area and areaPerBucket per spec

--- src/com/dstanton/PaintJob.java
+++ src/com/dstanton/PaintJob.java
@@ -12,4 +12,17 @@ public class PaintJob {
     public static int getBucketCount(double width, double height, double areaPerBucket) {
         return getBucketCount(width, height, areaPerBucket, 0);
     }
+
+    //Write another overloaded method named getBucketCount with 2 parameters namely,
+    // area and areaPerBucket (both of type double).
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area > 0 && areaPerBucket > 0) {
+            double width = Math.sqrt(area);
+            double height = width;
+            return getBucketCount(width, height, areaPerBucket, 0);
+        }
+
+        return - 1;
+    }
 }
